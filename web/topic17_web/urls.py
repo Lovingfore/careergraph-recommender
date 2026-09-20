0e9b9f9a2ee0ac44ba42be05046b146baa678558
Lovingfore <51793612+Lovingfore@users.noreply.github.com>
2026-09-20 08:18:08 +0800

    docs: annotate Django project routes

--- web/topic17_web/urls.py
+++ web/topic17_web/urls.py
@@ -1,6 +1,9 @@
+"""项目级 URL 入口：根路径下的所有请求统一交给 topic17_app 路由。"""
+
 from django.urls import include, path
 
 
 urlpatterns = [
+    # 应用路由包含首页、JSON API 以及两个 SVG 图谱资源。
     path("", include("web.topic17_app.urls")),
 ]
