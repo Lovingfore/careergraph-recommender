9a81e8fdb9edc6b798c57765559475a525b32c39
Lovingfore <51793612+Lovingfore@users.noreply.github.com>
2026-09-20 08:15:35 +0800

    docs: annotate Django app configuration

--- web/topic17_app/apps.py
+++ web/topic17_app/apps.py
@@ -1,7 +1,11 @@
+"""CareerGraph Web 应用的 Django 元数据配置。"""
+
 from django.apps import AppConfig
 
 
 class Topic17AppConfig(AppConfig):
+    """声明应用导入路径、默认主键类型和后台显示名称。"""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "web.topic17_app"
     verbose_name = "Topic 17 demo"
