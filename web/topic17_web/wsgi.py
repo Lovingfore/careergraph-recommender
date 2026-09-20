7670a86f47146aacd25b01ba1f298e55c311d351
Lovingfore <51793612+Lovingfore@users.noreply.github.com>
2026-09-20 08:18:36 +0800

    docs: annotate Django WSGI entrypoint

--- web/topic17_web/wsgi.py
+++ web/topic17_web/wsgi.py
@@ -1,7 +1,10 @@
+"""WSGI 应用入口，供生产级 WSGI 服务器加载 Django 应用对象。"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 
+# 外部服务器若未提供设置模块，则默认使用本项目配置。
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.topic17_web.settings")
 application = get_wsgi_application()
